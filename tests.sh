590f082b18a6fffabcaeab3648a000f6134bf773
Alan W Szlosek Jr <alan.szlosek@gmail.com>
2009-12-02 23:40:22 -0800

    Add lilypond logic to test script

--- tests.sh
+++ tests.sh
@@ -4,7 +4,11 @@ for file in *.music
 do
 	echo "Compiling $file"
 	python ../parser.py --midi < $file > miditext/$file.miditext
+	python ../parser.py --lilypond < $file > ly/$file.ly
 	~/downloads/midicomp-0.0.4/midicomp -c miditext/$file.miditext midi/$file.midi
 	timidity -c ~/composition/soundfont/timidity.cfg -Ow midi/$file.midi -o wav/$file.wav
+	cd ly
+	lilypond $file.ly --pdf
+	cd ..
 	echo "done"
 done
