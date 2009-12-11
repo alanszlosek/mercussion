09c355067cf25fd4694aaca1cc3216fe56d8aff9
Alan W Szlosek Jr <alan.szlosek@gmail.com>
2009-12-10 22:40:08 -0800

    Fix midi.sh script

--- midi.sh
+++ midi.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-python /home/alan/coding/_checkouts/mercussion/parser.py $1.music --midi > midi/$1.miditext
+python /home/alan/coding/_checkouts/mercussion/parser.py --midi < $1 > midi/$1.miditext
 cd midi
 ~/downloads/midicomp-0.0.4/midicomp -c $1.miditext $1.midi
 cd ..
