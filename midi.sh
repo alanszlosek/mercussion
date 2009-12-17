93049ee61b2a02fa151d7cc86d7f9ecb81bcc96e
Alan W Szlosek Jr <alan.szlosek@gmail.com>
2009-12-16 21:39:08 -0800

    Full paths in script

--- midi.sh
+++ midi.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-python /home/alan/coding/_checkouts/mercussion/parser.py --midi < $1 > midi/$1.miditext
-cd midi
-~/downloads/midicomp-0.0.4/midicomp -c $1.miditext $1.midi
-cd ..
+python /home/alan/coding/_checkouts/mercussion/parser.py --midi < $1 > /home/alan/coding/_checkouts/mercussion/testing/miditext/$1.miditext
+~/downloads/midicomp-0.0.4/midicomp -c /home/alan/coding/_checkouts/mercussion/testing/miditext/$1.miditext /home/alan/coding/_checkouts/mercussion/testing/midi/$1.midi
