d3b4083bdd1a87ee978c9b32dacf6ebec25b6dfc
Alan W Szlosek Jr <alan.szlosek@gmail.com>
2009-12-16 21:38:23 -0800

    Use full paths in scripts

--- pdf.sh
+++ pdf.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
-python /home/alan/coding/_checkouts/mercussion/parser.py --midi $1.music > ly/$1.ly
-cd pdf
-lilypond -f pdf ../ly/$1.ly
-rm *.ps
-cd ..
+python /home/alan/coding/_checkouts/mercussion/parser.py --lilypond < $1 > /home/alan/coding/_checkouts/mercussion/testing/ly/$1.ly
+lilypond -f pdf -o /home/alan/coding/_checkouts/mercussion/testing/pdf/$1 /home/alan/coding/_checkouts/mercussion/testing/ly/$1.ly
--- wav.sh
+++ wav.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-python /home/alan/coding/_checkouts/mercussion/parser.py $1.music --midi > midi/$1.miditext
-cd midi
-~/downloads/midicomp-0.0.4/midicomp -c $1.miditext $1.midi
-timidity -c ~/composition/soundfont/timidity.cfg -Ow $1.midi -o $1.wav
+python /home/alan/coding/_checkouts/mercussion/parser.py --midi < $1 > ~/coding/_checkouts/mercussion/testing/miditext/$1.miditext
+~/downloads/midicomp-0.0.4/midicomp -c ~/coding/_checkouts/mercussion/testing/miditext/$1.miditext ~/coding/_checkouts/mercussion/testing/midi/$1.midi
+timidity -c ~/composition/soundfont/timidity.cfg -Ow ~/coding/_checkouts/mercussion/testing/midi/$1.midi -o ~/coding/_checkouts/mercussion/testing/wav/$1.wav
