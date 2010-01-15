ca3312c54732377d2adb75ffa05150335b9f2883
Alan W Szlosek Jr <alan.szlosek@gmail.com>
2010-01-15 10:49:47 -0800

    Update paths on wav.sh script

--- wav.sh
+++ wav.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-python /home/alan/coding/_checkouts/mercussion/parser.py --midi < $1 > ~/coding/_checkouts/mercussion/testing/miditext/$1.miditext
-~/downloads/midicomp-0.0.4/midicomp -c ~/coding/_checkouts/mercussion/testing/miditext/$1.miditext ~/coding/_checkouts/mercussion/testing/midi/$1.midi
-timidity -c ~/composition/soundfont/timidity.cfg -Ow ~/coding/_checkouts/mercussion/testing/midi/$1.midi -o ~/coding/_checkouts/mercussion/testing/wav/$1.wav
+python /home/alan/coding/projects/mercussion/parser.py --midi < $1 > ~/coding/projects/mercussion/testing/miditext/$1.miditext
+~/downloads/midicomp-0.0.4/midicomp -c ~/coding/projects/mercussion/testing/miditext/$1.miditext ~/coding/projects/mercussion/testing/midi/$1.midi
+timidity -c ~/composition/soundfont/timidity.cfg -Ow ~/coding/projects/mercussion/testing/midi/$1.midi -o ~/coding/projects/mercussion/testing/wav/$1.wav
